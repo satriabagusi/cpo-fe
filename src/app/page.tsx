58bb77a6fce44533d2a0c3a03f1c19d5853814a8
hariadiaf <hariadi.adha@gmail.com>
2023-06-16 23:59:56 +0700

    install next ui and other modules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,3 @@
-"use client";
-// import { useState } from "react";
-// import styles from "./page.module.css";
-import { Button } from "@nextui-org/react";
-import { useState } from "react";
-
-export default function Home() {
-  const [page, setPage] = useState("aha");
-  return (
-    <main>
-      test {page}
-      <Button>tests</Button>
-    </main>
-  );
+export default function Index() {
+  return null;
 }
